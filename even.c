14e1732d58425d912242c09ddf66d34e99dad836
DManojprabhu <35231428+DManojprabhu@users.noreply.github.com>
2018-01-09 21:04:22 +0530

    oddoreven.c

---
+++ even.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("enter the number:");
+scanf("%d",&a);
+if(a%2)
+printf("the given number is even");
+else
+printf("the given number is odd");
+getch();
+}
