9819df23e2c195b4c25a5bd7bc48b435dfaa7f41
DManojprabhu <35231428+DManojprabhu@users.noreply.github.com>
2018-01-22 16:19:56 +0530

    Update primenum.c

--- primenum.c
+++ primenum.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 void main()
 {
-int n, i, flag = 0;
+int n, i, res= 0;
 printf("Enter a positive number: ");
 scanf("%d",&n);
 for(i=2; i<=n/2; ++i)
     {
         if(n%i==0)
         {
-            flag=1;
+            res=1;
             break;
         }
     }
 
-    if (flag==0)
+    if (res==0)
         printf("%d is a prime number.",n);
     else
         printf("%d is not a prime number.",n);
