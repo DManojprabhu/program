b673426a63dfaa2c500e8de7bb3d3cdd7efe6e27
DManojprabhu <35231428+DManojprabhu@users.noreply.github.com>
2018-02-09 15:49:04 +0530

    Create maximun element.c

---
+++ maximun element.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int n,m,i,gcd;
+	scanf("%d %d",&n,&m);
+	
+	for(i=2;i<=n&&i<=m;i++)
+	{
+              if(n%i==0&&m%i==0)
+              gcd=i;
+	}
+	printf("%d %d",gcd,gcd);
+}
