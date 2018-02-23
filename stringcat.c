e9a990ffba60fa32e638a8b278dd34f1352e5a3d
DManojprabhu <35231428+DManojprabhu@users.noreply.github.com>
2018-02-23 16:19:38 +0530

    Create stringcat.c

---
+++ stringcat.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+char  a[10],b[10];
+clrscr();
+printf("\tEnter the string one and string two:");
+scanf("\t\t%s%s",a,b);
+printf("----The concatenation of two strings -----\n%s%s",a,b);
+getch();
+}
