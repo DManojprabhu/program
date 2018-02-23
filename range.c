4b9ee5ae9ea79760be9a56a85737090ab2f3a5bb
DManojprabhu <35231428+DManojprabhu@users.noreply.github.com>
2018-02-23 16:25:32 +0530

    Create range.c

---
+++ range.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a;
+clrscr();
+printf("Enter the number:");
+scanf("%d",&a);
+if((a>1)&&(a<10))
+printf("yes");
+else
+printf("no");
+getch();
+}
