d907dd11a8c0d1cceb2ca03d24911146bbf06c5e
DManojprabhu <35231428+DManojprabhu@users.noreply.github.com>
2018-03-05 16:04:16 +0530

    Create productevenornot.c

---
+++ productevenornot.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a,b,c;
+clrscr();
+printf("Enter the two numbers:");
+scanf("%d%d",&a,&b);
+c=a+b;
+if(c%2==1)
+pritf("even");
+else
+printf("no");
+getch();
+}
