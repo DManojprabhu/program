170aa72d071424e23249f94d177c8e70a0df3486
DManojprabhu <35231428+DManojprabhu@users.noreply.github.com>
2018-03-09 15:49:01 +0530

    Create maxamong10.c

---
+++ maxamong10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+  main()
+  {
+    int a[10];
+    int i;
+    int max;
+    printf("Enter ten values:");
+
+    for (i = 0; i < 10; i++)
+    {
+    scanf("%d", &a[i]);
+    }
+    
+    max = a[0];
+    for (i = 0; i < 10; i++)
+    {
+if (a[i] > max)
+{
+max= a[i];
+    }
+    }
+    printf(" %d", max);
+    
+}
