6ff2d51fe0ee0a424df2c8e11a9a307ce1762b90
DManojprabhu <35231428+DManojprabhu@users.noreply.github.com>
2018-03-12 16:58:24 +0530

    Create prsq.c

---
+++ prsq.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	int a,b,c,i;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	c=a*b;
+	for(i=2;i<c;i++)
+	{
+		if(c==i*i);
+		{
+			printf("yes");
+			break;
+		}	
+	}
+	}
