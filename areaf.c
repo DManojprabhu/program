28db672a0eb68e5d21e8f735e0e26013d539c9e6
DManojprabhu <35231428+DManojprabhu@users.noreply.github.com>
2018-03-16 15:49:48 +0530

    Create areaf.c

---
+++ areaf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int b,h;
+float a;
+clrscr();
+printf("Enter the bredth:");
+scanf("%d",&b);
+printf("Enter the height:");
+scanf("%d",&h);
+a=((1/2)*h*b);
+printf("The area is:%d",a);
+getch();
+}
