b8d0aa36f1e6a3f8b87401263531514bc9b67eba
DManojprabhu <35231428+DManojprabhu@users.noreply.github.com>
2018-03-21 16:35:22 +0530

    Create alphanumeric..c

---
+++ alphanumeric..c
@@ -0,0 +1,20 @@
+ #include <stdio.h>
+ #include<conio.h>
+ #include<string.h>
+ void main()
+ {
+ char s[20],r[50];
+ int i,j;
+ printf("Enter the String:");
+ scanf("%s",&s);
+ for(i=0;s[i]!='\0';i++)
+ {
+ if(isdigit(s[i]))
+ {
+ r[j]=s[i];
+ j++;
+ }
+ }
+ printf("%s",r);
+ getch();
+ }
