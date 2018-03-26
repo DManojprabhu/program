f8ee3a6c3b59552a44ffeb470a5f351265bdd2c6
DManojprabhu <35231428+DManojprabhu@users.noreply.github.com>
2018-03-26 16:37:42 +0530

    Create 99.c

---
+++ 99.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a,b,c;
+ printf("enter the number:");
+ scanf("%d%d%d",&a,&b,&C);
+ op=a*b%c;
+ printf("the operation is %d",op);
+ getch();
+ }
